8b5fd2044f4864bdd53c5e0e160d1f138beed6db
nerobianchi <nerobianchi@gmail.com>
2017-10-27 01:01:25 +0300

    refactoring

---
+++ BowlingKata/Game.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingKata
+{
+    public class Game
+    {
+        private readonly FrameList frames = new FrameList();
+
+        public void Roll(int pins)
+        {
+            Frame currentFrame = frames.CurrentFrame();
+
+            currentFrame.Add(pins);
+        }
+
+        public int Score()
+        {
+            return this.frames.Sum(m => m.Score());
+        }
+    }
+
+    public class FrameList : List<Frame>
+    {
+        public FrameList()
+        {
+            this.Capacity = 10;
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                this.Add(new Frame(i, this));
+            }
+        }
+
+        public Frame CurrentFrame()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                var frame = this[i];
+                if (frame != null)
+                {
+                    if (!frame.IsCompleted())
+                    {
+                        return frame;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+
+    public class Frame
+    {
+        private Roll firstRoll;
+        private Roll secondRoll;
+        private readonly int index;
+        private readonly FrameList frameList;
+
+        public Frame(int index, FrameList frameList)
+        {
+            this.index = index;
+            this.frameList = frameList;
+        }
+
+        public void Add(int pins)
+        {
+            if (firstRoll == null)
+            {
+                firstRoll = new Roll(pins);
+            }
+            else if (secondRoll == null)
+            {
+                secondRoll = new Roll(pins);
+            }
+        }
+
+        public int Score()
+        {
+            var firstRollNumberOfPins = 0;
+            if (this.firstRoll != null)
+            {
+                firstRollNumberOfPins = this.firstRoll.NumberOfPins;
+            }
+            int secondRollNumberOfPins = 0;
+
+            if (this.secondRoll != null)
+            {
+                secondRollNumberOfPins = this.secondRoll.NumberOfPins;
+            }
+            var total = firstRollNumberOfPins + secondRollNumberOfPins;
+            if (total == 10)
+            {
+                var frame = this.frameList[this.index + 1];
+                if (frame != null) total += frame.FirstRollPins();
+            }
+            return total;
+        }
+
+        private int FirstRollPins()
+        {
+            return this.firstRoll?.NumberOfPins ?? 0;
+        }
+
+        public bool IsCompleted()
+        {
+            return this.firstRoll != null && this.secondRoll != null;
+        }
+    }
+
+    public class Roll
+    {
+        public int NumberOfPins { get; }
+
+        public Roll(int numberOfPins)
+        {
+            this.NumberOfPins = numberOfPins;
+        }
+    }
+}
--- BowlingKata/GameTests.cs
+++ BowlingKata/GameTests.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -69,8 +67,8 @@ namespace BowlingKata
         [Fact]
         public void given_first_frame_has_first_roll_is_5_pins_and_second_roll_is_5_pins_then_score_should_10()
         {
-            game.Roll(5);
-            game.Roll(5);
+            RollMany(2, 5);
+
             var score = game.Score();
             score.Should().Be(10);
         }
@@ -78,142 +76,10 @@ namespace BowlingKata
         [Fact]
         public void given_first_frame_has_5_pins_and_5_pins_and_second_frame_has_5_pins_then_score_should_20()
         {
-            game.Roll(5);
-            game.Roll(5);
-            game.Roll(5);
+            RollMany(3, 5);
+
             var score = game.Score();
             score.Should().Be(20);
-        }
-    }
-
-
-    public class Game
-    {
-        //private int totalScore;
-
-        private readonly FrameList frames = new FrameList();
-
-        public void Roll(int pins)
-        {
-            Frame currentFrame = frames.CurrentFrame();
-
-            currentFrame.Add(pins);
-//            frames.AddPinsToCurrentFrame(pins);
-
-            //totalScore += currentFrame.Score();
-        }
-
-        public int Score()
-        {
-            return this.frames.Sum(m => m.Score());
-        }
-    }
-
-    public class FrameList : List<Frame>
-    {
-        public FrameList()
-        {
-            this.Capacity = 10;
-            for (int i = 0; i < this.Capacity; i++)
-            {
-                this.Add(new Frame(i, this));
-            }
-        }
-
-        public Frame CurrentFrame()
-        {
-//            if (this.Count == 0)
-//            {
-//                this.Add(new Frame(this.Count + 1));
-//            }
-            for (int i = 0; i < this.Count; i++)
-            {
-                var frame = this[i];
-                if (frame != null)
-                {
-                    if (!frame.IsCompleted())
-                    {
-                        return frame;
-                    }
-                }
-            }
-
-//            if (this.Count < this.Capacity)
-//            {
-//                var frame = new Frame(this.Count + 1);
-//                this.Add(frame);
-//                return frame;
-//            }
-
-            return null;
-        }
-    }
-
-    public class Frame
-    {
-        private Roll firstRoll;
-        private Roll secondRoll;
-        private readonly int index;
-        private readonly FrameList frameList;
-
-        public Frame(int index, FrameList frameList)
-        {
-            this.index = index;
-            this.frameList = frameList;
-        }
-
-        public void Add(int pins)
-        {
-            if (firstRoll == null)
-            {
-                firstRoll = new Roll(pins);
-            }
-            else if (secondRoll == null)
-            {
-                secondRoll = new Roll(pins);
-            }
-        }
-
-        public int Score()
-        {
-            var firstRollNumberOfPins = 0;
-            if (this.firstRoll != null)
-            {
-                firstRollNumberOfPins = this.firstRoll.NumberOfPins;
-            }
-            int secondRollNumberOfPins = 0;
-
-            if (this.secondRoll != null)
-            {
-                secondRollNumberOfPins = this.secondRoll.NumberOfPins;
-            }
-            var total = firstRollNumberOfPins + secondRollNumberOfPins;
-            if (total == 10)
-            {
-                var frame = this.frameList[this.index + 1];
-                if (frame != null) total += frame.FirstRollPins();
-            }
-            return total;
-        }
-
-        private int FirstRollPins()
-        {
-            return this.firstRoll?.NumberOfPins ?? 0;
-        }
-
-        public bool IsCompleted()
-        {
-            return this.firstRoll != null && this.secondRoll != null;
-        }
-    }
-
-    public class Roll
-    {
-        public int NumberOfPins { get; }
-
-        public Roll(int numberOfPins)
-        {
-            this.NumberOfPins = numberOfPins;
         }
     }
 }
