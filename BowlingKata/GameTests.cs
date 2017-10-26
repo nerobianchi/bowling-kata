6f2833846b6ef91cc4fdd1767a6f3838daa7df1e
nerobianchi <nerobianchi@gmail.com>
2017-10-27 00:57:29 +0300

    roll 3     5/ 5        15 + 5 = 20 is passed

--- BowlingKata/GameTests.cs
+++ BowlingKata/GameTests.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -7,115 +7,105 @@ namespace BowlingKata
 {
     public class GameTests
     {
+        private readonly Game game;
+
+        public GameTests()
+        {
+            game = new Game();
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
         [Fact]
         public void given_a_game_which_all_rolls_are_0_then_score_should_be_0()
         {
             //arrange
-            Game game = new Game();
-            for (int i = 0; i < 20; i++)
-            {
-                game.Roll(0);
-                game.CurrentFrameIndex.Should().Be(i / 2 + 1);
-            }
 
-            //act
-            int score = game.Score();
+            RollMany(20, 0);
 
             //assert
-            score.Should().Be(0);
+            game.Score().Should().Be(0);
         }
+
 
         [Fact]
         public void given_a_game_which_all_rolls_are_1_then_score_should_be_20()
         {
             //arrange
-            Game game = new Game();
-            for (int i = 0; i < 20; i++)
-            {
-                game.Roll(1);
-                game.CurrentFrameIndex.Should().Be(i / 2 + 1);
-            }
-
-            //act
-            int score = game.Score();
+            RollMany(20, 1);
 
             //assert
-            score.Should().Be(20);
+            game.Score().Should().Be(20);
         }
 
         [Fact]
         public void given_a_game_which_frames_has_one_9_and_one_0_then_score_should_be_90()
         {
             //arrange
-            Game game = new Game();
             for (int i = 0; i < 10; i++)
             {
                 game.Roll(9);
                 game.Roll(0);
-                game.CurrentFrameIndex.Should().Be(i + 1);
             }
 
-            //act
-            int score = game.Score();
-
             //assert
-            score.Should().Be(90);
+            game.Score().Should().Be(90);
         }
 
-//        [Fact]
-//        public void given_first_frame_has_first_roll_is_5_pins_then_score_should_5()
-//        {
-//            Game game = new Game();
-//            game.Roll(5);
-//            var score = game.Score();
-//            score.Should().Be(5);
-//        }
-//
-//        [Fact]
-//        public void given_first_frame_has_first_roll_is_5_pins_and_second_roll_is_5_pins_then_score_should_10()
-//        {
-//            Game game = new Game();
-//            game.Roll(5);
-//            game.Roll(5);
-//            var score = game.Score();
-//            score.Should().Be(10);
-//        }
-//
-//        [Fact]
-//        public void given_first_frame_has_5_pins_and_5_pins_and_second_frame_has_5_pins_then_score_should_20()
-//        {
-//            Game game = new Game();
-//            game.Roll(5);
-//            game.Roll(5);
-//            game.Roll(5);
-//            var score = game.Score();
-//            score.Should().Be(20);
-//        }
+        [Fact]
+        public void given_first_frame_has_first_roll_is_5_pins_then_score_should_5()
+        {
+            game.Roll(5);
+            var score = game.Score();
+            score.Should().Be(5);
+        }
+
+        [Fact]
+        public void given_first_frame_has_first_roll_is_5_pins_and_second_roll_is_5_pins_then_score_should_10()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            var score = game.Score();
+            score.Should().Be(10);
+        }
+
+        [Fact]
+        public void given_first_frame_has_5_pins_and_5_pins_and_second_frame_has_5_pins_then_score_should_20()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            var score = game.Score();
+            score.Should().Be(20);
+        }
     }
 
 
     public class Game
     {
-        private int totalScore;
+        //private int totalScore;
 
         private readonly FrameList frames = new FrameList();
 
-        public int CurrentFrameIndex
-        {
-            get { return this.frames.CurrentFrame().Index; }
-        }
-
         public void Roll(int pins)
         {
-            frames.AddPinsToCurrentFrame(pins);
             Frame currentFrame = frames.CurrentFrame();
 
-            totalScore += currentFrame.Score();
+            currentFrame.Add(pins);
+//            frames.AddPinsToCurrentFrame(pins);
+
+            //totalScore += currentFrame.Score();
         }
 
         public int Score()
         {
-            return totalScore;
+            return this.frames.Sum(m => m.Score());
         }
     }
 
@@ -124,15 +114,19 @@ namespace BowlingKata
         public FrameList()
         {
             this.Capacity = 10;
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                this.Add(new Frame(i, this));
+            }
         }
 
         public Frame CurrentFrame()
         {
-            if (this.Count == 0)
-            {
-                return null;
-            }
-            for (int i = 0; i < this.Capacity; i++)
+//            if (this.Count == 0)
+//            {
+//                this.Add(new Frame(this.Count + 1));
+//            }
+            for (int i = 0; i < this.Count; i++)
             {
                 var frame = this[i];
                 if (frame != null)
@@ -144,20 +138,14 @@ namespace BowlingKata
                 }
             }
 
-            return null;
-        }
+//            if (this.Count < this.Capacity)
+//            {
+//                var frame = new Frame(this.Count + 1);
+//                this.Add(frame);
+//                return frame;
+//            }
 
-        public void AddPinsToCurrentFrame(int pins)
-        {
-            var currentFrame = this.CurrentFrame();
-            if (currentFrame == null)
-            {
-                this.Add(new Frame(pins,this.Count+1));
-            }
-            else
-            {
-                currentFrame.Add(pins);
-            }
+            return null;
         }
     }
 
@@ -165,44 +153,33 @@ namespace BowlingKata
     {
         private Roll firstRoll;
         private Roll secondRoll;
-        private int index;
+        private readonly int index;
+        private readonly FrameList frameList;
 
-        public Frame(int pins, int index)
+        public Frame(int index, FrameList frameList)
         {
             this.index = index;
-            this.firstRoll = new Roll(pins);
+            this.frameList = frameList;
         }
 
-        public int Index
-        {
-            get { return index; }
-        }
-
-        public bool Add(int numberOfPins)
+        public void Add(int pins)
         {
             if (firstRoll == null)
             {
-                firstRoll = new Roll(numberOfPins);
-                return true;
+                firstRoll = new Roll(pins);
             }
             else if (secondRoll == null)
             {
-                secondRoll = new Roll(numberOfPins);
-                return true;
-            }
-            else
-            {
-                return false;
+                secondRoll = new Roll(pins);
             }
         }
 
         public int Score()
         {
-            var roll = this.firstRoll;
             var firstRollNumberOfPins = 0;
-            if (roll != null)
+            if (this.firstRoll != null)
             {
-                firstRollNumberOfPins = roll.NumberOfPins;
+                firstRollNumberOfPins = this.firstRoll.NumberOfPins;
             }
             int secondRollNumberOfPins = 0;
 
@@ -210,7 +187,18 @@ namespace BowlingKata
             {
                 secondRollNumberOfPins = this.secondRoll.NumberOfPins;
             }
-            return firstRollNumberOfPins + secondRollNumberOfPins;
+            var total = firstRollNumberOfPins + secondRollNumberOfPins;
+            if (total == 10)
+            {
+                var frame = this.frameList[this.index + 1];
+                if (frame != null) total += frame.FirstRollPins();
+            }
+            return total;
+        }
+
+        private int FirstRollPins()
+        {
+            return this.firstRoll?.NumberOfPins ?? 0;
         }
 
         public bool IsCompleted()
